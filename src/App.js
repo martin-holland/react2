5937736198b74254cc7f048a54acfe09ca003f82
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-10-12 13:53:35 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
 
+const Box = (props) => {
+  return (
+      <div className = "box">
+        <h2>{props.name}</h2>
+        <p class="title">Title: {props.title}</p>
+        <p class="age">Age: {props.age}</p>
+      </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+        <header>
+        <div class="header-wrapper">
+        <h1>Logo</h1>
+        </div>
+        </header>
+      <main>
+        <div class="container">
+        <Box name="Maria" title="CEO" age="29"/>
+        <Box name="Kati" title="Developer" age="25"/>
+        <Box name="Karin" title="Designer" age="45"/>
+        <Box name="Martin" title="Wannabe" age="tooOld"/>
+            </div>
+        <footer>
+            <p>Copyrights</p>
+        </footer>
+    </main> 
     </div>
   );
 }
