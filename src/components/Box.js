20d322884e41f37e1f9f0da9f12560c1037abade
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-10-25 13:38:30 +0300

    added click handler within classes and functional components

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,9 +1,14 @@
 const Box = (props) => {
+
+  const addEventHandler = () => {
+    console.log("Wow from Box");
+  }
     return (
         <div className = "box">
           <h2>{props.name}</h2>
           <p class="title">Title: {props.title}</p>
           <p class="age">Age: {props.age}</p>
+          <button onClick={addEventHandler}>Click me</button>
         </div>
     )
   }
--- src/components/MainC.js
+++ src/components/MainC.js
@@ -10,12 +10,21 @@ class MainC extends Component {
         ]
     }
 
+    changeNameHandler = () => {
+        console.log("Wow within class")
+    }
+
     render() {
-        return (<main>
+        return (
+        <>
+        <main>
             <Box name = {this.state.persons[0].name} age={this.state.persons[0].age} title = {this.state.persons[0].title}/>
             <Box name = {this.state.persons[1].name} age={this.state.persons[1].age} title = {this.state.persons[1].title}/>
             <Box name = {this.state.persons[2].name} age={this.state.persons[2].age} title = {this.state.persons[2].title}/>
         </main>
+
+        <button onClick={this.changeNameHandler}>Click me</button>
+        </>
         );
     }
 }
